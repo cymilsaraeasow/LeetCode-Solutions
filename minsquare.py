d51474c10e5b746f8f49d8b027cbaf6d29a14afe
CYMIL SARA EASOW <cymilsara@gmail.com>
2025-08-21 09:16:19 +0530

    Add files via upload

---
+++ minsquare.py
@@ -0,0 +1,20 @@
+# ✅ Problem:minSquare
+# 🗓️ Date: 2025-08-21
+# 🧠 Language: Python
+import math
+N = int(input("Enter the number: "))
+
+def minSquares(N):
+    dp = [float('inf')]* (N+1)
+    dp[0] = 0
+    # dp gets filled with the answer to the ith number 
+    for i in range(1, N+1):
+        j = 1
+        # just check the minimum square less than the current val and it's number of squares
+        while j*j <= i:
+            dp[i] = min(dp[i], dp[i- j*j]+1)
+            j+=1
+
+    return dp[N]
+
+print(minSquares(N))
